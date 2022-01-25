008711e0abfb53be8848b8c4e0bc855c453712ef
Marquis Kurt <software@marquiskurt.net>
2022-01-25 11:41:19 +0100

    :wrench: Convert Capstone project into an Xcode project
    Although it would be a nice experiment to try building this project
    using Swift Playgrounds 4/Xcode, it is currently limited to iOS.
    
    After a careful assessment of the project objectives, I have decided
    that I will instead perform this experiment more traditionally with
    and Xcode project set up to be multiplatform, targeting both iOS
    and macOS simiulatenously.
    
    This also opens the door to using other Apple-provided technologies
    such as Core Data, CloudKit, and Sign In with Apple. These toolkits
    may not be used in the project necessarily, but Xcode will support
    these features, unlike SP4 at the moment.
    
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

---
+++ Shared/Shout.swift
@@ -0,0 +1,23 @@
+//
+//  Shout.swift
+//  Shared
+//
+//  Created by Marquis Kurt on 25/1/22.
+//  This file is part of Codename Shout.
+//
+//  Codename Shout is non-violent software: you can use, redistribute, and/or modify it under the terms of the CNPLv7+
+//  as found in the LICENSE file in the source code root directory or at <https://git.pixie.town/thufie/npl-builder>.
+//
+//  Codename Shout comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law. See the CNPL for
+//  details.
+
+import SwiftUI
+
+@main
+struct Shout: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
