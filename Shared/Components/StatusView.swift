0e7ef171056e7f70f4f78d907929695649d8d36d
Marquis Kurt <software@marquiskurt.net>
2022-02-11 12:44:43 -0500

    :lipstick: Make general improvements to StatusView
    StatusView is more configurable with different options for where to
    place the creation date and the profile image. To adjust some of the
    options, view modifiers for StatusView are available; this route was
    chosen to declutter the view's init call.
    
    Additionally, a new date formatter has been introduced to parse dates
    written in the Mastodon format.

--- Shared/Components/StatusView.swift
+++ Shared/Components/StatusView.swift
@@ -16,39 +16,158 @@ import Foundation
 import SwiftUI
 import Chica
 
+// MARK: - Status View
 
+/// A view that displays a status created by a user.
 struct StatusView: View {
+
+    /// An enumeration representing the different position options for the status creation date.
+    enum DatePlacement {
+
+        /// The default position. Typically, this appears as the last line, under the status's content.
+        case `default`
+
+        /// Under the author's username, before the status's content.
+        case underUsername
+    }
+
+    /// An enumeration representing the different position options for the author's profile image.
+    enum ProfileImagePlacement {
+
+        /// Next to the author's name, towards the left side.
+        case byAuthorName
+
+        /// Next to the entire view on the left side.
+        ///
+        /// Ideal for messages and list entries in a master-detail view.
+        case byEntireView
+    }
+
+    /// The status data to render in this view.
     @State var status: Status
-    @State var truncateLines: Int? = nil
+
+    /// The maximum number of lines to display in the status's content.
+    ///
+    /// If this value is `nil`, all lines will be displayed.
+    @State fileprivate var truncateLines: Int?
+
+    /// The placement configuration for the date.
+    @State fileprivate var datePlacement: DatePlacement
+
+    /// The placement configuration for the author's profile image.
+    @State fileprivate var profileImagePlacement: ProfileImagePlacement
+
+    init(status: Status) {
+        self.init(status: status, truncateLines: nil, datePlacement: .default, profileImagePlacement: .byAuthorName)
+    }
+
+    fileprivate init(
+        status: Status,
+        truncateLines: Int?,
+        datePlacement: DatePlacement,
+        profileImagePlacement: ProfileImagePlacement
+    ) {
+        self.status = status
+        self.truncateLines = truncateLines
+        self.datePlacement = datePlacement
+        self.profileImagePlacement = profileImagePlacement
+    }
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 4) {
-            HStack {
-                Image(systemName: "person.circle")
-                    .imageScale(.large)
-                VStack {
-                    HStack {
-                        Text(status.account.displayName)
-                            .bold()
-                        Text("(@\(status.account.acct))")
-                            .foregroundColor(.secondary)
-                    }
-                    .font(.system(.title3, design: .rounded))
-                }
+        HStack(spacing: 16) {
+            if profileImagePlacement == .byEntireView {
+                authorImage
             }
-            Text(status.content.toPlainText())
-                .lineLimit(truncateLines)
-            HStack {
-                Spacer()
-                Text(status.createdAt)
-                    .foregroundColor(.secondary)
+            VStack(alignment: .leading, spacing: 4) {
+                HStack {
+                    if profileImagePlacement == .byAuthorName {
+                        authorImage
+                    }
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text(status.account.displayName)
+                                .bold()
+                            Text("(@\(status.account.acct))")
+                                .foregroundColor(.secondary)
+                        }
+                        .font(.system(.title3, design: .rounded))
+                        if datePlacement == .underUsername {
+                            statusCreationDate
+                        }
+                    }
+                }
+                Text(status.content.toPlainText())
+                    .lineLimit(truncateLines)
+                if datePlacement == .default {
+                    HStack {
+                        Spacer()
+                        statusCreationDate
+                    }
+                }
             }
         }
         .font(.system(.body, design: .rounded))
-        .padding()
+    }
+
+    private var statusCreationDate: some View {
+        Text(
+            DateFormatter.mastodon.date(from: status.createdAt)!,
+            format: .relative(presentation: .named)
+        )
+            .foregroundColor(.secondary)
+    }
+
+    private var authorImage: some View {
+        AsyncImage(url: .init(string: status.account.avatarStatic)) { image in
+            image
+                .resizable()
+                .scaledToFit()
+                .clipShape(Circle())
+        } placeholder: {
+            Image(systemName: "person.circle")
+                .imageScale(.large)
+        }
+        .frame(width: 48, height: 48)
     }
 }
 
+// MARK: - Modifiers
+
+extension StatusView {
+
+    /// Sets the maximum number of lines to display the status's content at.
+    func lineLimit(_ lineLimit: Int) -> StatusView {
+        StatusView(
+            status: self.status,
+            truncateLines: lineLimit,
+            datePlacement: self.datePlacement,
+            profileImagePlacement:
+                self.profileImagePlacement
+        )
+    }
+
+    /// Sets the placement for where the author's profile image will appear.
+    func profilePlacement(_ profilePlacement: ProfileImagePlacement) -> StatusView {
+        StatusView(
+            status: self.status,
+            truncateLines: self.truncateLines,
+            datePlacement: self.datePlacement,
+            profileImagePlacement: profilePlacement
+        )
+    }
+
+    /// Sets the placement for where the status's creation date will appear.
+    func datePlacement(_ datePlacement: DatePlacement) -> StatusView {
+        StatusView(
+            status: self.status,
+            truncateLines: self.truncateLines,
+            datePlacement: datePlacement,
+            profileImagePlacement: self.profileImagePlacement
+        )
+    }
+}
+
+// MARK: - Previews
 struct StatusView_Previews: PreviewProvider {
     static var statusData: Status? = try! JSONDecoder.decodeFromResource(from: "Status")
 
@@ -56,11 +175,16 @@ struct StatusView_Previews: PreviewProvider {
         Group {
 
             StatusView(status: statusData!)
+                .datePlacement(.underUsername)
+                .padding()
                 .frame(maxWidth: 550)
 
             List {
                 ForEach(1..<5) { _ in
-                    StatusView(status: statusData!, truncateLines: 2)
+                    StatusView(status: statusData!)
+                        .lineLimit(2)
+                        .profilePlacement(.byEntireView)
+                        .datePlacement(.default)
                 }
             }
             .frame(maxWidth: 400)
