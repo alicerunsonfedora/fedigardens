95d665b792c74c0697591e11dd7dfd5259dd90b4
Marquis Kurt <software@marquiskurt.net>
2023-01-22 12:43:51 -0500

    :wrench: Update Dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -15,14 +15,37 @@
 import Danger
 let danger = Danger()
 
-let PR_THRESHOLD = 20 // swiftlint:disable:this identifier_name
+var suggestsChanges = false
 
+// MARK: - Configuration Variables
+let PR_FILE_THRESHOLD = 20 // swiftlint:disable:this identifier_name
+
+// MARK: - Big PR check
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
-if editedFiles.count > 20 {
+if editedFiles.count > PR_FILE_THRESHOLD {
     warn(
         "This appears to be a big PR. If this PR contains multiple features or tickets, please "
         + "consider splitting them up for easier review."
     )
+    suggestsChanges = true
 }
 
-SwiftLint.lint()
+if danger.git.deletedFiles.count > PR_FILE_THRESHOLD {
+    warn("This PR contains \(danger.git.deletedFiles.count) files that were deleted.")
+    suggestsChanges = true
+}
+
+// MARK: - SwiftLint
+let linterWarnings = SwiftLint.lint()
+if !linterWarnings.isEmpty {
+    suggestsChanges = true
+}
+
+// MARK: - Congratulatory message
+let randomCongraulatoryMessages = [
+    "Good on ya!", "Great work!", "Excellent work!", "Good job!", ";^)", "Perfection.", "Congrats!"
+]
+if !suggestsChanges {
+    markdown("Code looks great. " + (randomCongraulatoryMessages.randomElement() ?? "Good job!"))
+    markdown("Note that a successful Danger check does *not* mean an automatic PR approval.")
+}
