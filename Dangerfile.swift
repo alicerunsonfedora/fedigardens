0d31b964cd6602e1ebd26d510f3ac5d964058c37
Marquis Kurt <software@marquiskurt.net>
2023-01-22 12:02:57 -0500

    :wrench: Update Dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,5 +1,13 @@
 import Danger 
 let danger = Danger()
 
+let PR_THRESHOLD = 20
+
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
-message("These files have changed: \(editedFiles.joined(separator: ", "))")
+if editedFiles.count > 20 {
+    warn(
+        "This appears to be a big PR. If this PR contains multiple features or tickets, please consider splitting them up for easier review."
+    )
+}
+
+SwiftLint.lint()
