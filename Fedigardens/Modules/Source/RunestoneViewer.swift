cab293f1a463564e6ac6e9bfda3e77bec0c3c125
Marquis Kurt <software@marquiskurt.net>
2023-05-05 22:17:42 -0400

    :art: Add missing license header

--- Fedigardens/Modules/Source/RunestoneViewer.swift
+++ Fedigardens/Modules/Source/RunestoneViewer.swift
@@ -4,6 +4,13 @@
 //
 //  Created by Marquis Kurt on 5/5/23.
 //
+//  This file is part of Fedigardens.
+//
+//  Fedigardens is non-violent software: you can use, redistribute, and/or modify it under the terms of the CNPLv7+
+//  as found in the LICENSE file in the source code root directory or at <https://git.pixie.town/thufie/npl-builder>.
+//
+//  Fedigardens comes with ABSOLUTELY NO WARRANTY, to the extent permitted by applicable law. See the CNPL for
+//  details.
 
 import SwiftUI
 import Runestone
