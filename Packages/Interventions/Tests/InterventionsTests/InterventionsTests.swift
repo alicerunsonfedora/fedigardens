a2cc4d72449d71c18dffb9e66455cb43bb990c98
Marquis Kurt <software@marquiskurt.net>
2023-06-26 09:17:09 -0400

    :test_tube: Add unit tests, helpers
    Unit tests for the intervention flow have been added to begin work on
    the implementation for the module.
    
    FlowKitTestSupport also includes some new helpers to write common test
    tasks such as checking that a state matches and waiting for an event to
    finish emitting.

--- Packages/Interventions/Tests/InterventionsTests/InterventionsTests.swift
+++ Packages/Interventions/Tests/InterventionsTests/InterventionsTests.swift
@@ -3,5 +3,53 @@ import FlowKitTestSupport
 import XCTest
 
 final class InterventionFlowTests: XCTestCase, StatefulTestCase {
-    
+    typealias TestableFlow = InterventionFlow
+
+    var flow: InterventionFlow?
+
+    override func setUp() async throws {
+        self.flow = InterventionFlow()
+    }
+
+    override func tearDown() async throws {
+        self.flow = nil
+    }
+
+    func testEmitAuthorizationRequest() async throws {
+        await withCheckedFlow { currentFlow in
+            let initialRequestDate = Date.now
+            await currentFlow.emitAndWait(event: .requestIntervention,
+                                          in: self,
+                                          forPeriod: 2,
+                                          timeout: 5,
+                                          message: "Authorization request")
+            currentFlow.expectState(matches: .requestedIntervention(initialRequestDate))
+        }
+    }
+
+    func testEmitAuthorizationCompletion() async throws {
+        await withCheckedFlow { currentFlow in
+            await currentFlow.emitAndWait(event: .requestIntervention,
+                                          in: self,
+                                          forPeriod: 2,
+                                          timeout: 5,
+                                          message: "Authorization request")
+            let context = InterventionAuthorizationContext(allowedTimeInterval: 5, allowedFetchSize: 10)
+            let capturedDate = Date.now
+            await currentFlow.emit(.authorizeIntervention(capturedDate, context: context))
+            currentFlow.expectState(matches: .authorizedIntervention(capturedDate, context: context))
+        }
+    }
+
+    func testEmitReset() async throws {
+        await withCheckedFlow { currentFlow in
+            await currentFlow.emitAndWait(event: .requestIntervention,
+                                          in: self,
+                                          forPeriod: 2,
+                                          timeout: 5,
+                                          message: "Authorization request")
+            await currentFlow.emit(.reset)
+            currentFlow.expectState(matches: .initial)
+        }
+    }
 }
