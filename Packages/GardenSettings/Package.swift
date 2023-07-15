160e36a5bff5a7c8054b0ff232718ed26bbe7294
Marquis Kurt <software@marquiskurt.net>
2023-07-15 11:24:37 -0400

    :construction: Create GardenSettings
    This module will contain user preferences and customizations.

---
+++ Packages/GardenSettings/Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GardenSettings",
+    defaultLocalization: "en",
+    platforms: [.macOS(.v13), .iOS(.v16)],
+    products: [
+        .library(
+            name: "GardenSettings",
+            targets: ["GardenSettings"]),
+    ],
+    dependencies: [
+        .package(name: "FlowKit", path: "../FlowKit")
+    ],
+    targets: [
+        .target(
+            name: "GardenSettings",
+            dependencies: [.product(name: "FlowKit", package: "FlowKit")]),
+        .testTarget(
+            name: "GardenSettingsTests",
+            dependencies: [
+                "GardenSettings",
+                .product(name: "FlowKitTestSupport", package: "FlowKit")
+            ]),
+    ]
+)
