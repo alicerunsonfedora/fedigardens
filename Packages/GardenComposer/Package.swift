1a817eebb805e4098f39ebb70e59b6a05b8fe4e9
Marquis Kurt <software@marquiskurt.net>
2023-08-01 18:56:38 -0400

    :construction: Create GardenComposer package
    This package will contain the source code for the Composer feature in
    Fedigardens.

---
+++ Packages/GardenComposer/Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GardenComposer",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(
+            name: "GardenComposer",
+            targets: ["GardenComposer"]),
+    ],
+    dependencies: [
+        .package(name: "Alice", path: "../Alice"),
+        .package(name: "FlowKit", path: "../FlowKit")
+    ],
+    targets: [
+        .target(
+            name: "GardenComposer",
+            dependencies: ["Alice", "FlowKit"],
+            resources: [
+                .process("Resources")
+            ]),
+        .testTarget(
+            name: "GardenComposerTests",
+            dependencies: [
+                "GardenComposer",
+                .product(name: "AliceMockingbird", package: "Alice"),
+                .product(name: "FlowKitTestSupport", package: "FlowKit")
+            ]),
+    ]
+)
