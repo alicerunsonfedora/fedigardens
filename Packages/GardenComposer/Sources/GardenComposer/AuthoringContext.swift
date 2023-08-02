f5856e28120c0d3d83710d2ea94e7006cc01579f
Marquis Kurt <software@marquiskurt.net>
2023-08-02 19:45:43 -0400

    :memo: Document current public APIs

--- Packages/GardenComposer/Sources/GardenComposer/AuthoringContext.swift
+++ Packages/GardenComposer/Sources/GardenComposer/AuthoringContext.swift
@@ -16,20 +16,54 @@ import Alice
 import Foundation
 import GardenSettings
 
+/// A structure that defines a context for which an authoring scene can be triggered.
+///
+/// An authoring context can represent various actions, such as replying to a discussion, creating a quote, or editing a
+/// discussion.
 public struct AuthoringContext: Codable, Hashable, Identifiable {
     @GardenSetting(key: .defaultVisibility, defaultVisibility: .public)
     static var defaultPostVisibility = PostVisibility.public
 
+    /// The unique identifier for this context.
     public var id = UUID()
+
+    /// The ID of the discussion that is being edited.
+    ///
+    /// If the context represents a discussion that is being edited, its ID will be used to send a `PUT` request.
     public var editablePostID: String = ""
+
+    /// The URI of the discussion that is being forwarded.
+    ///
+    /// If a context includes a quote discussion, its originating URI will live here.
     public var forwardingURI: String = ""
+
+    /// The participants in the discussion.
+    ///
+    /// Participants are represented as account handles (`@johnsmith`).
     public var participants: String = ""
+
+    /// The prefilled contents of the discussion's message.
+    ///
+    /// This is commonly used in editing contexts or where a reply is fabricated with prefilled content.
     public var prefilledText: String = ""
+
+    /// The ID of the discussion this context will reply to.
     public var replyingToID: String = ""
+
+    /// The suggested post visibility that this discussion will have.
+    ///
+    /// By default, this is set to the user's default new discussion visibility.
     public var visibility: PostVisibility = AuthoringContext.defaultPostVisibility
+
+    /// The time at which the context's poll will expire.
+    ///
+    /// This is typically a `TimeInterval` represented as a string, if the context includes a poll.
     public var pollExpiration: String = ""
+
+    /// A comma-separated list of the options the context's poll includes.
     public var pollOptions: String = ""
 
+    /// Creates an authoring context that can be passed around.
     public init(editablePostID: String = "",
                 forwardingURI: String = "",
                 participants: String = "",
@@ -51,6 +85,8 @@ public struct AuthoringContext: Codable, Hashable, Identifiable {
 }
 
 public extension AuthoringContext {
+    /// Creates a context from a supplied URL, starting with `gardens://create`.
+    /// - Parameter url: The URL that will be parsed to generate an authoring context.
     init?(from url: URL) {
         guard url.absoluteString.starts(with: "gardens://create"), let params = url.queryParameters else { return nil }
         self = AuthoringContext(
@@ -65,6 +101,7 @@ public extension AuthoringContext {
         )
     }
 
+    /// Constructs a URL from the current context, encoding the current values as necessary.
     func constructURL() -> URL? {
         guard let url = URL(string: "gardens://create") else { return nil }
         var query = [URLQueryItem]()
@@ -103,6 +140,11 @@ public extension AuthoringContext {
 }
 
 public extension ComposerDraft {
+    /// Creates a draft by reading an authoring context, taking into account a prompt discussion and the current author.
+    ///
+    /// - Parameter authoringContext: The authoring context the draft will be created from.
+    /// - Parameter prompt: The discussion the context is prompted by, either a reply or quote.
+    /// - Parameter author: The current author of the context.
     init(authoringContext: AuthoringContext, promptedBy prompt: Status, author: Account) {
         var constructedText = authoringContext.prefilledText
         var draftPoll: ComposerDraftPoll?
@@ -127,6 +169,7 @@ public extension ComposerDraft {
 }
 
 public extension URL {
+    /// Creates an instance of a URL object from an authoring context.
     init?(from context: AuthoringContext) {
         guard let url = context.constructURL() else { return nil }
         self = url
